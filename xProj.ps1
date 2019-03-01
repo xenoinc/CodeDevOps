62778ef3581c1a1a0abdc3a079863d869f65bb80
Damian Suess <Damian.Suess@xenoinc.com>
2019-03-01 07:07:40 +0700

    Added xProj templates

---
+++ xProj.ps1
@@ -0,0 +1,16 @@
+<#
+  Copyright 2019 Xeno Innovations, Inc.
+  xProject DevOps dependency sync system
+  2019-02-27
+  
+  TODO:
+    [ ] Place all framework templates in their own folders vs flat
+    [ ] Make it work
+
+  Sample:
+  xProj GetFramework    - Refresh all project rules
+  xProj get stylecop    - Only refresh stylecop files
+  xProj get editorcfg
+  xProj get git         - GitIgnore
+  xProj get MSBuild     - MSBuild proj files
+#>
