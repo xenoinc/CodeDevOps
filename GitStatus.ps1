c33891a1711d586a8dfa1299890e562c89401ef3
Damian Suess <Damian.Suess@xenoinc.com>
2019-03-21 05:28:59 -0400

    Added GitPrune with sync, GitStatus, added notes

---
+++ GitStatus.ps1
@@ -0,0 +1,19 @@
+<#
+  Git commandline helpers for PowerShell
+  Author: Damian Suess
+  Revision: 1 - 2019-01-16
+
+  Usage:
+    GitStaus    - Returns the counts of each status type
+
+  Change Log:
+    2019-0321 - 0.1 - Created
+#>
+
+# Include Files --------
+. "GitHelpers.ps1";
+
+# Our code -------------
+GitStatus;
+
+Write-Host "GitStatus completed." -ForegroundColor Green;
